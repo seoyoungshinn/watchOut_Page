482e3b603cfc9f8b50bf1345daed54b80df05a1e
b22016645 <byeolstar12@naver.com>
2022-07-26 02:12:56 +0900

    bootmap.html 추가용

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ def welcome():
 def hello_world2():
     return render_template('drawMarker.html')
 
+@app.route('/bootmap')
+def boot():
+    return render_template('bootmap.html')
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", post="5000")
 
