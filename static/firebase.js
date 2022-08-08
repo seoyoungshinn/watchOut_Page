53dd9fa135a0fc328128aa7658be9636f12757ff
Seul-Seul <ejsong428@nate.com>
2022-08-08 11:16:53 +0900

    덮

--- static/firebase.js
+++ static/firebase.js
@@ -1,25 +1,4 @@
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js";
-// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-analytics.js";
-// 	  const firebaseConfig = {
-// 		apiKey: "AIzaSyAdfT9fZDJFN4II45NITCqBGpguzVrls5U",
-// 		authDomain: "watchout-5da32.firebaseapp.com",
-// 		databaseURL: "https://watchout-5da32-default-rtdb.firebaseio.com",
-// 		projectId: "watchout-5da32",
-// 		storageBucket: "watchout-5da32.appspot.com",
-// 		messagingSenderId: "559553548409",
-// 		appId: "1:559553548409:web:647b935251b767e69c84a8",
-// 		measurementId: "G-YTFRDPWZGD"
-// 	  };
-	
-// 	  // Initialize Firebase
-// 	  const app = initializeApp(firebaseConfig);
-
-//  const db = firebase.firestore();
-//  db.collection('Favorites').get().then((결과)=>{
-//     console.log(결과);
-// })
-
-
+/*firebase설정키*/
 const firebaseConfig = {
     apiKey: "AIzaSyAdfT9fZDJFN4II45NITCqBGpguzVrls5U",
     authDomain: "watchout-5da32.firebaseapp.com",
@@ -31,15 +10,16 @@ const firebaseConfig = {
     measurementId: "G-YTFRDPWZGD"
   };
 
+  /*firestore에서 db가져옴*/
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
-function showDB(){
+function showYoungShimHouse(){
         var docRef = db.collection("PersonalData").doc("kstL3GdcSqbnZcNsFjm669zUFih2").collection("Favorites").doc("영심이네");
-
         docRef.get().then((doc) => {
             if (doc.exists) {
-                console.log("Document data:", doc.data());
+                console.log("영심이네:", doc.data());
+                console.log("주소" + doc.data().address);
             } else {
                 // doc.data() will be undefined in this case
                 console.log("No such document!");
@@ -47,4 +27,16 @@ function showDB(){
         }).catch((error) => {
             console.log("Error getting document:", error);
         }); 
+}
+
+function showAllFavorites(){
+    db.collection("PersonalData").doc("kstL3GdcSqbnZcNsFjm669zUFih2").collection("Favorites").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log(doc.id, " => ", doc.data());
+        });
+    });
+}
+
+function fromFavorites(){
+    
 }
