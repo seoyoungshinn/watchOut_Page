5d06216382150bfda06da5c355e38de50b42508e
Seul-Seul <ejsong428@nate.com>
2022-09-02 14:49:59 +0900

    json가져와서 지도띄우기 분기점요소들 다 가져오기

--- static/js/onlysimulation.js
+++ static/js/onlysimulation.js
@@ -14,30 +14,77 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 /* ------------------RealTimeDatabase-------------------- */
-//json으로 저장된 dong아래 lat이랑 lon배열 가져옴
-function getAllDataFromRealTimeDatabase(des) {
-    const dbRef = firebase.database().ref(des);
+var lat_arr,lon_arr,turn_arr,cross_arr,elevator_arr,overpass_arr,underpass_arr;
+
+function getAllDataFromRealTimeDatabase(num) {
+    const dbRef = firebase.database().ref(num.toString());
     dbRef.get().then((snapshot) => {
         if (snapshot.exists()) {
-            console.log(snapshot.val().elevator);
-
-            drawResMap(snapshot.val().lat,snapshot.val().lon); //tmap.js내 지도 그리는 함수 
+            var value = snapshot.val();
+            showdesAndTime(value);
+            drawResMap(value.lat,value.lon); //tmap.js내 지도 그리는 함수 
         } else {
             console.log("No data available");
         }
+
+        if(value.turn[0] != "n"){ //turnpoint없으면 첫번째 값이 n임
+            turn_arr = value.turn;
+        }else{
+            console.log("분기점이 없는 길임");
+            turn_arr = 0;
+        }
+       
+        if(value.cross[0] != "n"){ //turnpoint없으면 첫번째 값이 n임
+            cross_arr = value.cross;
+        }else{
+            console.log("횡단보도가 없는 길임");
+            cross_arr = 0;
+        }
+        
+
+        if(value.elevator[0] != "n"){ //turnpoint없으면 첫번째 값이 n임
+            elevator_arr = value.elevator;
+        }else{
+            console.log("엘베가 없는 길임");
+            elevator_arr = 0;
+        }
+
+        if(value.overpass[0] != "n"){ //turnpoint없으면 첫번째 값이 n임
+            overpass_arr = value.overpass;
+        }else{
+            console.log("육교가 없는 길임");
+            overpass_arr = 0;
+        }
+        
+        if(value.underpass[0] != "n"){ //turnpoint없으면 첫번째 값이 n임
+            underpass_arr = value.underpass;
+        }else{
+            console.log("지하보도가 없는 길임");
+            underpass_arr = 0;
+        }
+
+        return value;
+    })
+    .then((value)=>{
+            $('#startBtn').css('visibility','visible');
     }).catch((error) => {
         console.error(error);
     });
 }
 
-function showSimulName(history) {
+function showdesAndTime(value) {
     var id = document.getElementById('simultime');
-    var atime = history.departureTime;
-    var dtime = history.arrivedTime;
-    id.innerHTML = dtime+" ~ "+atime+"의 History";
+    var startTime = value.start_time;
+    var endTime = value.end_time;
+    id.textContent = startTime+" ~ "+endTime+"의 History";
 
     var id2 = document.getElementById('simulname');
-    var aname = history.arrivedName;
-    var dname = history.dpName;
-    id2.innerHTML = dname+" -> "+aname;
+    var startName = value.start;
+    var endName = value.end;
+    id2.textContent =startName+" -> "+endName;
+}
+
+function startSimulation(){
+    //시뮬레이션시작
+    console.log(turn_arr);
 }
